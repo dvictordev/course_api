dd29fb53aedef646cb1d495a1bef5bca2b50ca50
Victor Duarte <victor.d.dev@gmail.com>
2024-05-06 11:35:35 -0300

    create project & database connection

---
+++ src/test/java/com/victor/course_api/CourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.course_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
