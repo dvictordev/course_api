df05627527c07f9151685caff1dcc09fbdb6e06c
victor duarte <victor.d.dev@gmail.com>
2024-07-23 10:51:25 -0300

    feat: add not null validation to category

--- src/main/java/com/victor/course_api/modules/course/entities/CourseEntity.java
+++ src/main/java/com/victor/course_api/modules/course/entities/CourseEntity.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.NonNull;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -21,6 +23,7 @@ public class CourseEntity {
     @Pattern(regexp = "\\S+", message = "O campo [name] não deve conter espaço.")
     private String name;
 
+    @NotNull
     private String category;
 
     private Boolean active;
